316c6eb25c5bf2ef445e17e7a5583a1c974eeaff
yossefdahan <yossefdahan1@gmail.com>
2024-02-28 15:57:35 +0200

    add downLoadImg option

--- js/controler/meme-editor-controler.js
+++ js/controler/meme-editor-controler.js
@@ -129,3 +129,13 @@ function getEvPos(ev) {
     }
     return pos
 }
+
+function init() {
+    gElCanvas = document.getElementById('my-canvas')
+    gCtx = gElCanvas.getContext('2d')
+}
+
+function downloadImg(elLink) {
+    const imgContent = gElCanvas.toDataURL('image/jpeg') // image/jpeg the default format
+    elLink.href = imgContent
+}
