980077c45b17a895a7fd3a74e3381e1acd0b85a1
yossefdahan <yossefdahan1@gmail.com>
2024-02-28 12:32:11 +0200

    add renderMeme() function

--- js/controler/meme-editor-controler.js
+++ js/controler/meme-editor-controler.js
@@ -1,1 +1,55 @@
 'use strict'
+
+
+let gElCanvas
+let gCtx
+
+function onInit() {
+
+    gElCanvas = document.querySelector('canvas')
+    gCtx = gElCanvas.getContext('2d')
+    renderMeme()
+
+}
+
+
+function renderMeme() {
+
+    const memeImgs = getMemesImgs()
+    const memeText = getMemesText()
+
+    var selectedImgId = memeText.selectedImgId
+
+    var selectedLineIdx = memeText.selectedLineIdx
+    var selectedLine = memeText.lines[selectedLineIdx]
+
+    const selectedImg = memeImgs.find(img => img.id === selectedImgId)
+    var { id, url, keywords } = selectedImg
+
+    drawImg(selectedImg, selectedLine)
+    renderText(selectedLine)
+}
+
+
+function renderText(selectedLine) {
+    gCtx.fillStile = selectedLine.color
+    gCtx.font = selectedLine.size + 'px Arial'
+    gCtx.textAlign = 'center'
+    gCtx.fillText(selectedLine.txt, gElCanvas.width / 2, gElCanvas.height / 2)
+}
+
+function drawImg(selectedImg, selectedLine) {
+    const img = new Image()
+    img.src = selectedImg.url
+    // console.log(img);
+    img.onload = () => {
+        gCtx.drawImage(img, 0, 0, img.naturalWidth, img.naturalHeight)
+        renderText(selectedLine)
+    }
+    coverCanvasWithImg(img)
+}
+
+function coverCanvasWithImg(elImg) {
+    gElCanvas.height = (elImg.naturalHeight / elImg.naturalWidth) * gElCanvas.width
+    gCtx.drawImage(elImg, 0, 0, gElCanvas.width, gElCanvas.height)
+}
--- js/service/meme-service.js
+++ js/service/meme-service.js
@@ -7,12 +7,27 @@ var gImgs = [{
 }]
 
 var gMeme = {
-    selectedImgId: 5, selectedLineIdx: 0, lines:
-        [{
-            txt: 'I sometimes eat Falafel', size: 20, color: 'red'
-        }]
+    selectedImgId: 1,
+    selectedLineIdx: 0,
+    lines: [{
+
+        txt: 'I sometimes eat Falafel',
+        size: 20,
+        color: 'red',
+    }]
 }
 
 var gKeywordSearchCountMap = {
     'funny': 12, 'cat': 16, 'baby': 2
 }
+
+
+function getMemesImgs() {
+    return gImgs
+}
+
+function getMemesText() {
+    return gMeme
+}
+
+
